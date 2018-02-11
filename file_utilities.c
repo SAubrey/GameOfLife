bf0346f3febda3997450705d26120581cef3e1b5
Sean Aubrey <seanpaubrey@gmail.com>
2018-02-11 17:51:57 -0500

    Saves and Loads from file
    +Save to text file
    +Load from text file
    -Set up to take user input to name the text file,
    but currently is hardcoded to "state.txt" for saving
    and loading.
    -Time to implement game logic!

--- file_utilities.c
+++ file_utilities.c
@@ -1,48 +1,66 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <io.h>
-//#include <file_utilities.h>
-//
+
 // Created by Sean Aubrey on 2/9/2018.
-//
 
-/*
- * file_utilities.c saves and loads from txt file.
- * Save increments through the 2D array, using periods to
- * denote the end of a row.
- * Load then parses a string of integers,
- * where row = the number of periods, and col = the number
- * of integers before the first period.
+/* file_utilities.c saves and loads from txt file.
+ * Readfile returns the size of the array read in if successful,
+ * otherwise 1. Buffer is a ptr to an array, not a 2D array.
  *
+ * Both currently only use a preset file name.
  */
-int read_file( char* filename, char **buffer ) {
+int read_file(char *filename, char **buffer ) {
     FILE *fin;
-    fin = open(filename, "r");
-    int row, col;
+    filename = "state.txt";
+    fin = fopen(filename, "r");
 
-
-    if (filename == NULL) {
+    int size = 0;
+    // Iterate through buffer to determine row/col sizes.
+    if (filename == NULL || fin == NULL) {
         printf("File does not exist!\n");
-    }
+        fclose(fin);
+        return 1;
 
-    // Read from file
-    while (!feof(fin)) {
-
-
-    }
-
-    // Insert into board
-    int i, j;
-    for (i = 0; i < row; row++) {
-        for (j = 0; j < col; col++) {
-
+    } else {
+        fseek(fin, 0L, SEEK_END); // move to end
+        size = (int)ftell(fin); // get size of file
+        if (size <= 1) {
+            printf("Nothing in file. \n");
+            fclose(fin);
+            return 1;
         }
+
+        *buffer = (char*)malloc(size * sizeof(char));
+        fseek(fin, 0L, SEEK_SET); // Return fin to start.
+        fread(*buffer, sizeof(char), size, fin);
+
+        fclose(fin);
+        return size;
     }
 }
-int write_file( char* filename, char *buffer, int size) {
+
+/*
+ * If we have the size, which is row * col, then we can read the first row
+ * and determine the number of columns. col = size/row
+ * These are generic functions that can be used to read/write any array.
+ * Row/col handling must occur in main.
+ */
+int write_file(const char* filename, char *buffer, int size) {
     FILE *fout;
-    fout = open(filename, "w");
+    //fout = fopen(filename, "w");
+    fout = fopen("state.txt", "w");
 
+    if (filename == NULL || fout == NULL) {
+        printf("Not a valid file name!\n");
+        fclose(fout);
+        return 1;
+    }
 
-
+    int i;
+    for (i = 0; i < size; i++) {
+        fputc(buffer[i], fout);
+    }
+    fclose(fout);
+    return 0;
 }
--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "file_utilities.h"
 /*
  * file_utilities.c saves and loads from txt file.
  * Save increments through the 2D array, using periods to
@@ -28,28 +30,33 @@
  */
 
 /*  https://www.geeksforgeeks.org/dynamically-allocate-2d-array-c/  */
-/*  https://www.geeksforgeeks.org/enumeration-enum-c/  */
+/*  https://stackoverflow.com/questions/18688971/c-char-array-initialization  */
 
-void createBoard(int row, int col);
-void printBoard();
+void createBoard(int r, int c);
+void printBoard(int r, int c);
 void loadBoard();
-void saveBoard();
+int saveBoard();
 void updateBoard(int n);
 int promptUser();
 int freeBoard();
 
-int row, col;
+int row = 0;
+int col = 0;
 int generations;
+char usrIn[10];
 /*
  * Using board, determine the next game state onto board2, then overwrite
  * board with tempBoard.
  */
 char **board; // 2D array
-char *fileName;
+//char *fileName;
+char fileName[30];
 
+/*
+ * TODO: Get rid of as many global variables as possible.
+ * TODO: Toggle option for showing prompt text?
+ */
 int main(int argc, char const *argv[]) {
-
-    createBoard(10, 10);
     /*
     if (argc != 2) {
         printf("Usage: Number_of_rows Number_of_columns\n");
@@ -60,33 +67,48 @@ int main(int argc, char const *argv[]) {
         row = atoi(argv[1]);
         col = atoi(argv[2]);
     }
-    while (1) {
-        switch (promptUser()) {
 
+    while (1) {
+        //fflush(stdin);
+        switch (promptUser()) {
             case 1: // Update 1 generation
                 updateBoard(1);
                 break;
             case 2: // Update n generations
                 printf("Number of generations: \n");
-                scanf("%d", &generations);
+                fflush(stdin);
+                fgets(usrIn, sizeof(usrIn), stdin);
+                generations = atoi(usrIn);
                 updateBoard(generations);
                 break;
             case 3: // Save
-                printf("Save as?: \n");
-                scanf("%c", &fileName);
-                //write_file(fileName, *board, size);
+                //memset(fileName, 0, strlen(fileName)); // zeros out array
+                printf("Save as?: (All files currently route to 'state.txt', "
+                               "any input will do.)\n");
+                fflush(stdin);
+                fgets(fileName, 31, stdin);
+                saveBoard();
                 break;
             case 4: // Load
-                printf("Enter file name: \n");
-                scanf("%c", &fileName);
-                //read_file(fileName, **board);
+                //memset(fileName, 0, strlen(fileName)); // zeros out array
+                //fileName = (char*)malloc(30 * sizeof(char));
+                printf("Load file name: (All files currently route to 'state.txt', "
+                               "any input will do.)\n");
+                fflush(stdin);
+                fgets(fileName, 31, stdin);
+                loadBoard();
                 break;
             case 5: // Exit
                 printf("Destroying world. How can you sleep at night? \n");
+                freeBoard();
                 exit(0);
+            case 6:
+                //printBoard();
+                break;
+            default:
+                break;
         }
     }
-    return 0;
 }
 
 /*
@@ -101,11 +123,12 @@ void updateBoard(int n) {
     for (i; i < n; i++) {
 
     }
-    printBoard();
+    printBoard(row, col);
 }
 
 int promptUser() {
-    int selection;
+    int selection = 0;
+    char c[2];
 
     printf("What would you like to do?\n");
     printf("(1) Generate\n");
@@ -115,29 +138,103 @@ int promptUser() {
     printf("(5) Quit\n");
     //printf("(5) Spawn life at coordinates: X Y\n");
 
-    scanf("%d", &selection);
+    fflush(stdin); // Clears troublesome '\n' character left in console.
+    fgets(c, 2, stdin);
+    selection = atoi(c); //strtol?
     return selection;
 }
 
-void printBoard() {
-    // print board
+void printBoard(int r, int c) {
+    row = r;
+    col = c;
     int i, j;
     for (i = 0; i < row; i++) {
         for (j = 0; j < col; j++) {
-            printf("%d ", board[i][j]);
+            printf("%c ", board[i][j]);
         }
         printf("\n");
     }
 }
 
+int saveBoard() {
+    int i, j = 0;
+    int size = 0;
+    char *str;
+    str = (char*)malloc((row * (col + 1)) * sizeof(char));
+    if (board == NULL) {
+        printf("There's no board to save.\n");
+        return 1;
+    }
+
+    for (i = 0; i < row; i++) {
+        for (j = 0; j < col; j++) {
+
+            str[size] = board[i][j];
+            size++;
+        }
+        str[size] = '.';
+        size++;
+    }
+    write_file(fileName, str, size);
+    printf("Game saved.\n");
+}
+
+/*
+ * Number of periods = row size
+ * Characters before a period = col size
+ * read_file assigns one long string to str,
+ * we must then take this string and used periods to
+ * determine row & col, and then fill in the game board.
+ */
+void loadBoard() {
+    char *str;
+    int size;
+    if ((size = read_file(fileName, &str)) > 1) {
+        // Board now contains periods, which will be used
+        // to determine row ends, and must then be removed.
+        row = 0;
+        col = 0;
+        int i, j = 0;
+
+        // Get row size
+        while (str[col] != '.') {
+            col++;
+        }
+        for (i = 0; i < size; i++) {
+            if (str[i] == '.') {
+                row++;
+            }
+        }
+
+        freeBoard(); // Clear out previous board, if it exists
+        createBoard(row, col);
+        /* Fill the board. Since row and col are accurate to the
+         * board size, but we are iterating through a string that still
+         * contains periods to denote the end of rows, then we must
+         * add one to col in the loop to account for each extra char.
+         */
+        int n = 0;
+        for (i = 0; i < row; i++) {
+            for (j = 0; j < col + 1; j++) {
+                if (str[n] != '.') {
+                    board[i][j] = str[n];
+                }
+                n++;
+            }
+        }
+        printBoard(row, col);
+    }
+    free(str);
+}
+
 /*Board is the address to an array of addresses,
  * each of which is an address to the first element
  * of an array.
- *
  */
-void createBoard(int row, int col) {
+void createBoard(const int r, const int c) {
     int i, j;
-
+    row = r;
+    col = c;
     board = (char **)malloc(row * sizeof(char*));
     for (i = 0; i < row; i++) {
         board[i] = (char*)malloc(col * sizeof(char));
@@ -145,17 +242,17 @@ void createBoard(int row, int col) {
 
     for (i = 0; i < row; i++) {
         for (j = 0; j < col; j++) {
-            board[i][j] = '_';
+            board[i][j] = '-';
         }
     }
 }
 
 int freeBoard() {
     int i;
-    for (i = 0; i < row; i++) {
-        free(board[i]);
+    if (board != NULL) {
+        for (i = 0; i < row; i++) {
+            free(board[i]);
+        }
+        free(board);
     }
-    free(board);
 }
-
-
